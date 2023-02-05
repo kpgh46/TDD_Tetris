a7dd4a8da388671b432ea58381ecc9a7e569fa09
Kevin McPeak <kevmc46@gmail.com>
2023-02-04 20:37:23 -0500

    refactored to include beforeEach

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ let createTetrisBlock = (first, second, third, fourth) => {
 	const tetrisBlock = [first, second, third, fourth];
 	return tetrisBlock;
 };
-// let test = createTetrisBlock([0, 1], [0, 2], [0, 3], [0, 4]);
-// console.log(test[0][1]);
 
 module.exports = createTetrisBlock;
--- tests/grid.test.js
+++ tests/grid.test.js
@@ -1,12 +1,15 @@
 const createTetrisBlock = require("../index");
 
 describe("create tetris blocks", () => {
+	let tetrisBlock;
+	beforeEach(() => {
+		tetrisBlock = createTetrisBlock([0, 1], [0, 2], [0, 3], [0, 4]);
+	});
+
 	test("each block has a length", () => {
-		const tetrisBlock = createTetrisBlock();
 		expect(tetrisBlock.length).toBeGreaterThan(0);
 	});
 	test("the first array value is a number", () => {
-		const tetrisBlock = createTetrisBlock([0, 1], [0, 2], [0, 3], [0, 4]);
 		expect(tetrisBlock[0][1]).toEqual(1);
 	});
 });
